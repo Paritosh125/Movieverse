c765346c3cd700d61cae03a077ed0d61f5625ac2
Paritosh125 <sandhanparitosh.com>
2025-10-06 23:35:31 +0530

    Footer change

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 text-gray-400 py-5 text-center text-sm border-t border-gray-700 relative">
             <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center gap-2">
                 <span>&copy; {new Date().getFullYear()} MovieVerse</span>
-                <span>Developed by Paritosh Sandhan</span>
+                {/* <span>Developed by Paritosh Sandhan</span> */}
                 <span className="text-gray-500">All rights reserved</span>
             </div>
 
